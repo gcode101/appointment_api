7e210a0e50667930d90451a5cb730202f88694e0
Guelmis Cortina <cguelmis@yahoo.com>
2015-11-24 00:27:26 -0500

    added the abillity to list appointments using queries and filters. Also wrote integration tests

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,8 +1,17 @@
 module API
 	class AppointmentsController < ApplicationController
+
 		def index
 			appointments = Appointment.all
+			if last_name = params[:last_name]
+				appointments = appointments.where(last_name: last_name)
+			end
 			render json: appointments, status: 200
+		end
+
+		def show
+			appointment = Appointment.find(params[:id])
+			render json: appointment, status: :ok
 		end
 	end
 end
--- test/integration/listing_appointments_test.rb
+++ test/integration/listing_appointments_test.rb
@@ -2,11 +2,32 @@ require 'test_helper'
 
 class ListingAppointmentsTest < ActionDispatch::IntegrationTest
 
-	setup { host! 'api.example.com' }
+	setup { host! 'api.appointment-dev.com:3000' }
 
 	test 'returns list of all appointments' do
 		get '/appointments'
 		assert_equal 200, response.status
 		refute_empty response.body
+	end
+
+	test 'returns appointments filtered by last_name' do
+		bayley = Appointment.create!(start_time: "11/1/13 9:30", end_time: "11/1/13 9:35", first_name: "bayley", last_name: "keller")
+		brenda = Appointment.create!(start_time: "11/1/13 11:00", end_time: "11/1/13 11:05", first_name: "brenda", last_name: "hardy")
+		get '/appointments?last_name=keller'
+		assert_equal 200, response.status
+
+		appointments = json(response.body)
+		names = appointments.collect { |z| z[:last_name]}
+		assert_includes names, 'keller'
+		refute_includes names, 'hardy'
+	end
+
+	test 'returns appointment by id' do
+		appointment = Appointment.create!(start_time: '11/23/2015 11:39')
+		get "/appointments/#{appointment.id}"
+		assert_equal 200, response.status
+
+		appointment_response = json(response.body)
+		assert_equal appointment.start_time, appointment_response[:start_time]
 	end
 end
